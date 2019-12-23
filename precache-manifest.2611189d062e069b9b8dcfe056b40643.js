285be46739e318d1e785920052877ad147ab074d
Mike Whitlaw <mike.whitlaw@gmail.com>
2019-12-23 09:35:44 -0500

    Updates

---
+++ precache-manifest.2611189d062e069b9b8dcfe056b40643.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a62a373016ffb8c2df925cbb64764a00",
+    "url": "/monster-book/index.html"
+  },
+  {
+    "revision": "af0316997d90ad4a3325",
+    "url": "/monster-book/static/css/main.77135998.chunk.css"
+  },
+  {
+    "revision": "cd46350ec4b3a59b27b9",
+    "url": "/monster-book/static/js/2.0bc0afcb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-book/static/js/2.0bc0afcb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "af0316997d90ad4a3325",
+    "url": "/monster-book/static/js/main.fb271104.chunk.js"
+  },
+  {
+    "revision": "59c96b1f58692f71f3fa",
+    "url": "/monster-book/static/js/runtime-main.63072bd3.js"
+  }
+]);
